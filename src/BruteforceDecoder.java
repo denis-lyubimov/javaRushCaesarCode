b3d68f92b0fed175188c20c43e5c914cc6cdbae6
Denis Lyubimov <some-mailbox@gmail.com>
2022-06-20 05:04:13 +0300

    splited "all-code-in-one file" into several files

---
+++ src/BruteforceDecoder.java
@@ -0,0 +1,6 @@
+public class BruteforceDecoder extends Converter implements Decodable, Hackable {
+    @Override
+    public String Decode() {
+        return "";
+    }
+}
--- src/BruteforceDecorder.java
+++
@@ -1,6 +0,0 @@
-public class BruteforceDecorder extends Converter implements Decodable, Hackable {
-    @Override
-    public String Decode() {
-        return "";
-    }
-}
